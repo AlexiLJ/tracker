9d067580050e99f83cb9320b30b7b4d4995c2093
Alex Leiko <leiko.oleksandr@gmail.com>
2021-11-01 23:35:11 +0100

    draft

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import subprocess as sub
 import platform 
+import psutil
+import time
 
 print(platform.system())
-
 proc = input('Run process: ')
 if platform.system() == 'Windows':
     procces_run = sub.Popen([proc]) #, stdout=sub.PIPE, encoding="utf-8")
- 
-    print(sub.run(f'Get-Process -Id ${procces_run.pid}'))
+    p = psutil.Process(procces_run.pid)
+    while True:
+        time.sleep(1)
+        p.cpu_percent()
+    # print(sub.run(f'Get-Process -Id ${procces_run.pid}'))
     CPU_time = sub.run(f'typeperf "\{procces_run.pid}\% Processor Time" -si 10 -sc 5', shell=True)
     print(sub.run(f'Get-Process {procces_run})'))
 # if __name__ == '__name__':
